45ca5640798ab0121865dc740a1862a4e029b852
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-07-28 14:20:55 -1000

    trying to launch landing page on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 
-function App() {
+//Layouts
+import LandingPageLayout from './layouts/Landingpage'
+
+//Pages
+import HomePage from './pages/HomePage';
+
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Switch>
+    <Route path='/'>
+    <LandingPageLayout heading="Tailwind CSS">
+      <HomePage />
+    </LandingPageLayout>
+    </Route>
+  </Switch>
   );
-}
+};
 
 export default App;
---
+++ src/components/Cards/index.js
@@ -0,0 +1,88 @@
+
+import React from "react";
+import Photo from "./../../assets/landscape.jpeg";
+
+const Cards = ({ ...otherProps }) => {
+  return (
+    <div className="flex flex-row flex-wrap py-14 justify-center">
+      
+        <div
+          className="
+        w-full lg:w-3/6 xl:w-4/12
+        px-2.5 mb-5
+        "
+        >
+          <div className="bg-white">
+          <div>
+            <img src={Photo} alt='' className="w-full" />
+          </div>
+
+          <div className="p-2.5">
+            <ul>
+              <li className="list-none">
+                <span className="font-bold text-base">Title</span>
+              </li>
+
+              <li lassName="list-none">
+                <span className="text-base">Description</span>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </div>
+      
+
+        <div
+          className="
+        w-full lg:w-3/6 xl:w-4/12
+        px-2.5 mb-5
+        "
+        >
+          <div className="bg-white">
+          <div>
+            <img src={Photo} alt='' className="w-full" />
+          </div>
+
+          <div className="p-2.5">
+            <ul>
+              <li className="list-none">
+                <span className="font-bold text-base">Title</span>
+              </li>
+
+              <li lassName="list-none">
+                <span className="text-base">Description</span>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </div>
+
+        <div
+          className="
+        w-full lg:w-3/6 xl:w-4/12
+        px-2.5 mb-5
+        "
+        >
+          <div className="bg-white">
+          <div>
+            <img src={Photo} alt='' className="w-full" />
+          </div>
+
+          <div className="p-2.5">
+            <ul>
+              <li className="list-none">
+                <span className="font-bold text-base">Title</span>
+              </li>
+
+              <li lassName="list-none">
+                <span className="text-base">Description</span>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Cards;
