2df6169b2d79d7cfcbb6e4f525f790e3af50f339
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-07-28 16:46:11 -1000

    please work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import Logo from "./../../assets/tailwind-css-logo.svg";
+import Logo from "./../../assets/apple-touch-icon.png";
 import { Link } from "react-router-dom";
 
 const Header = ({ ...otherProps }) => {
